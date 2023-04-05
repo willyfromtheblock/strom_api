d81d798a66ffa8d7be952567b4375b8909782304
Willy <willy@coinerella.com>
2023-04-05 16:43:47 +0200

    add tests for endpoints

--- test/app_test_with_env.dart
+++ test/app_test_with_env.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:pvpc_server/price_watcher.dart';
 import 'package:pvpc_server/tools/http_wrapper.dart';
 import 'package:pvpc_server/tools/logger.dart';
+import 'package:pvpc_server/tools/price_zone.dart';
 
 import '../bin/pvpc_server.dart' as app;
 import 'package:test/test.dart';
@@ -9,7 +12,7 @@ import 'package:test/test.dart';
 void main() {
   late Dio dio;
   setUp(() {
-    String restURL = 'localhost:3001';
+    String restURL = 'http://localhost:3001';
 
     dio = Dio(
       BaseOptions(baseUrl: restURL),
@@ -34,12 +37,92 @@ void main() {
     },
   );
 
-  group(
-    'RestServer',
-    () {
-      test('get price now for peninsular', () async {
-        print(await dio.get('/price/0/peninsular'));
-      });
-    },
-  );
+  group('RestServer', () {
+    group(
+      'price-now',
+      () {
+        test('get all available zones', () async {
+          for (var zone in PriceZone.values) {
+            await dio.get('/price/0/${zone.name}');
+          }
+        });
+
+        test('get non existing zone', () async {
+          expect(
+            () async => await dio.get(
+              '/price/0/berlin',
+            ),
+            throwsA(
+              isA<DioError>(),
+            ),
+          );
+        });
+
+        test('get price in one hour in peninsular', () async {
+          final time = DateTime.now().add(Duration(hours: 1));
+          final secondsSinceEpoch = time.millisecondsSinceEpoch ~/ 1000;
+          await dio.get(
+            '/price/$secondsSinceEpoch/peninsular',
+          );
+        });
+
+        test('get price in two days in peninsular', () async {
+          final time = DateTime.now().add(Duration(days: 2));
+          final secondsSinceEpoch = time.millisecondsSinceEpoch ~/ 1000;
+
+          expect(
+            () async => await dio.get(
+              '/price/$secondsSinceEpoch/peninsular',
+            ),
+            throwsA(
+              isA<DioError>(),
+            ),
+          );
+        });
+      },
+    );
+    group(
+      'price-average',
+      () {
+        test('get all available zones', () async {
+          for (var zone in PriceZone.values) {
+            await dio.get('/price-average/0/${zone.name}');
+          }
+        });
+
+        test('get non existing zone', () async {
+          expect(
+            () async => await dio.get(
+              '/price-average/0/berlin',
+            ),
+            throwsA(
+              isA<DioError>(),
+            ),
+          );
+        });
+
+        test('get price-average in one hour in peninsular', () async {
+          final time = DateTime.now().add(Duration(hours: 1));
+          final secondsSinceEpoch = time.millisecondsSinceEpoch ~/ 1000;
+          await dio.get(
+            '/price-average/$secondsSinceEpoch/peninsular',
+          );
+        });
+
+        test('get price-average in two days in peninsular', () async {
+          final time = DateTime.now().add(Duration(days: 2));
+          final secondsSinceEpoch = time.millisecondsSinceEpoch ~/ 1000;
+
+          expect(
+            () async => await dio.get(
+              '/price-average/$secondsSinceEpoch/peninsular',
+            ),
+            throwsA(
+              isA<DioError>(),
+            ),
+          );
+        });
+      },
+    );
+  });
 }
