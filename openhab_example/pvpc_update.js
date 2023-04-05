8103e1f8b27ab5aeabd15e8442f74fa14b128c80
Willy <willy@coinerella.com>
2023-04-05 21:16:03 +0200

    update openhab example

--- openhab_example/pvpc_update.js
+++ openhab_example/pvpc_update.js
@@ -1,14 +1,16 @@
-const response = actions.HTTP.sendHttpGetRequest('https://pvpc-hourly-spanish-energy-prices-api.p.rapidapi.com/price/0/peninsular', {
-    'X-RapidAPI-Key': 'your-rapid-api-key',
-    'X-RapidAPI-Host': 'your-rapid-api-host'
-}, 10000);
-const parsed = JSON.parse(response);
+(function () {
+    const response = actions.HTTP.sendHttpGetRequest('https://pvpc-hourly-spanish-energy-prices-api.p.rapidapi.com/price/0/peninsular', {
+        'X-RapidAPI-Key': 'your-rapid-api-key',
+        'X-RapidAPI-Host': 'your-rapid-api-host'
+    }, 10000);
+    const parsed = JSON.parse(response);
 
-const currentPriceItem = items.getItem('PVPCServer_current_price');
-currentPriceItem.postUpdate(parseFloat(parsed['price']));
+    const currentPriceItem = items.getItem('PVPCServer_current_price');
+    currentPriceItem.postUpdate(parseFloat(parsed['price']));
 
-const currentPriceRating = items.getItem('PVPCServer_Currentpricerating');
-currentPriceRating.postUpdate(parsed['price_rating']);
+    const currentPriceRating = items.getItem('PVPCServer_Currentpricerating');
+    currentPriceRating.postUpdate(parsed['price_rating']);
 
-const currentPriceTimeStamp = items.getItem('PVPCServer_Timestampforcurrentprice');
-currentPriceTimeStamp.postUpdate(parsed['time']);
+    const currentPriceTimeStamp = items.getItem('PVPCServer_Timestampforcurrentprice');
+    currentPriceTimeStamp.postUpdate(parsed['time']);
+})();
