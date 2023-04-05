cccc97aa79b42b2883de953c151d851dc0d4d2f4
Willy <willy@coinerella.com>
2023-04-05 19:05:14 +0200

    add 10s timeout

--- openhab_example/pvpc_update.js
+++ openhab_example/pvpc_update.js
@@ -1,7 +1,7 @@
 const response = actions.HTTP.sendHttpGetRequest('https://pvpc-hourly-spanish-energy-prices-api.p.rapidapi.com/price/0/peninsular', {
     'X-RapidAPI-Key': 'your-rapid-api-key',
     'X-RapidAPI-Host': 'your-rapid-api-host'
-});
+}, 10000);
 const parsed = JSON.parse(response);
 
 const currentPriceItem = items.getItem('PVPCServer_current_price');
